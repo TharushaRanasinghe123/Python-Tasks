41434f9c94b80d034272e7f58ad35494adcfd877
R.M.T.Pradeepani <pradeepani_rmt_e23@engug.ruh.ac.lk>
2025-06-02 12:20:30 +0530

    Refactor web scraper to collect book data and save to JSON file

--- Python-Learning-Map-5/scraper_project/main.py
+++ Python-Learning-Map-5/scraper_project/main.py
@@ -1,19 +1,52 @@
-# main.py
+# import requests
+# from bs4 import BeautifulSoup
+# import json
+
+# all_data = []
+
+# for page in range(1, 4):
+#     url = f"https://quotes.toscrape.com/page/{page}/"
+#     res = requests.get(url)
+#     soup = BeautifulSoup(res.text, "html.parser")
+#     quotes = soup.find_all("div", class_="quote")
+#     for q in quotes:
+#         text = q.find("span", class_="text").text
+#         author = q.find("small", class_="author").text
+#         all_data.append({"quote": text, "author": author})
+
+# with open("quotes.json", "w") as file:
+#     json.dump(all_data, file, indent=2)
+
 import requests
 from bs4 import BeautifulSoup
 import json
 
-all_data = []
+all_books = []
 
+# Iterate through the first 3 pages
 for page in range(1, 4):
-    url = f"https://quotes.toscrape.com/page/{page}/"
+    url = f"http://books.toscrape.com/catalogue/page-{page}.html"
     res = requests.get(url)
     soup = BeautifulSoup(res.text, "html.parser")
-    quotes = soup.find_all("div", class_="quote")
-    for q in quotes:
-        text = q.find("span", class_="text").text
-        author = q.find("small", class_="author").text
-        all_data.append({"quote": text, "author": author})
 
-with open("quotes.json", "w") as file:
-    json.dump(all_data, file, indent=2)
+    # Find all book articles
+    books = soup.find_all("article", class_="product_pod")
+
+    for book in books:
+        title = book.h3.a["title"]
+        price = book.find("p", class_="price_color").text
+        rating_class = book.find("p", class_="star-rating")["class"]
+        rating = rating_class[1] if len(rating_class) > 1 else "No rating"
+
+        all_books.append({
+            "title": title,
+            "price": price,
+            "rating": rating
+        })
+
+# Save to JSON file
+with open("books.json", "w", encoding="utf-8") as file:
+    json.dump(all_books, file, indent=2, ensure_ascii=False)
+
+
+print("Done! Data saved to books.json")
